703f8fdd4bca6bdce5fc4d9442833b81626dc1ea
gioqareli <kareligiorgi121@gmail.com>
2025-02-15 10:13:32 -0800

    first commit

---
+++ Day 64/classwork/lesson64.js
@@ -0,0 +1,17 @@
+
+var username = prompt("Enter Your Name!:")
+var usersurname = prompt("Enter your Surname!:")
+
+console.log( " Hello" + username + " " + usersurname )
+alert ("hello" + username + usersurname)
+
+var num_1 = prompt (" Enter Random Number!:")
+var num_2 = prompt (" Enter Random Number!:")
+
+console.log(  num_1 + num_2)
+console.log(  num_1 - num_2)
+console.log(  num_1 * num_2)
+console.log(  num_1 / num_2)
+
+var name = prompt(" Enter Your Name!:")
+alert ("welcome" + name)
